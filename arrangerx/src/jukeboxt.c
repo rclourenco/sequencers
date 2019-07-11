afae5662247eaaf42c6fd42ab1091a92c89f7553
Renato <renato@asus-rcl>
2019-07-11 01:01:35 +0100

    wip

--- arrangerx/src/jukeboxt.c
+++ arrangerx/src/jukeboxt.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/resource.h>
 #include <Xm/List.h>
+#include <Xm/ScrolledW.h>
 #include <Xm/BulletinB.h>
 #include <Xm/RowColumn.h>
 #include <X11/IntrinsicP.h> // IntrinsicP.h has some faster macros than Intrinsic.h
@@ -73,6 +75,19 @@ void proc_exit();
 
 unsigned long into = 0;
 
+void catch_events(Widget shell, XtPointer client_data, XEvent *event, Boolean *ctd);
+
+pid_t cpid = -1;
+
+void shutdown() {
+	if (gSignalStatus==0)
+		return;
+	if (cpid>0) {
+		kill(cpid, SIGTERM);
+	}
+	printf("Shutdown!\n");
+}
+
 int main (int argc, char **argv)
 {
 	int status = 0;
@@ -81,6 +96,8 @@ int main (int argc, char **argv)
 	XmStringTable    str_list;
 	Window w1, w2;
 	signal (SIGCHLD, proc_exit);
+
+	atexit(shutdown);
 
 	if (argc>1) {
 		playlist_file = argv[1];
@@ -97,7 +114,7 @@ int main (int argc, char **argv)
 	main_widget = XtOpenApplication(&app, "MIDI Jukebox", NULL, 0, &argc, argv, NULL, sessionShellWidgetClass, NULL, 0);
 	//--- Make it the desired size 
 //	XtMakeResizeRequest(main_widget, 400, 100, &xx, &yy);
-
+        
         bboard = XmCreateRowColumn(main_widget, "bboard", NULL, 0);
 
     /* Set up a translation table that captures "Resize" events 
@@ -117,14 +134,22 @@ int main (int argc, char **argv)
 	XtFree ((char *) str_list);
 
         XtManageChild (list);
-        
-	bboard2 = XmCreateBulletinBoard (bboard, "player", NULL, 0);
+      
+        Arg args[4];	
+        n = 0;
+        XtSetArg (args[n], XmNwidth, 500);                   n++;
+        XtSetArg (args[n], XmNheight, 300);                  n++;
+        XtSetArg (args[n], XmNscrollingPolicy, XmAUTOMATIC); n++;    
+	bboard2 = XmCreateScrolledWindow(bboard, "player", args, n);
 	XtManageChild(bboard2);
 
 	XtManageChild (bboard);
-        XtMakeResizeRequest(bboard2, 400, 400, &xx, &yy);
+       // XtMakeResizeRequest(bboard2, 400, 400, &xx, &yy);
 
-        XtAddCallback (list, XmNdefaultActionCallback, sel_callback, NULL);	
+        XtAddCallback (list, XmNdefaultActionCallback, sel_callback, NULL);
+        
+	XtAddEventHandler(bboard2, SubstructureNotifyMask|SubstructureRedirectMask, False, catch_events, NULL);
+
 	//--- Realize the main widget 
 	XtRealizeWidget(main_widget);
 	w1 = XtWindow(main_widget);
@@ -136,6 +161,39 @@ int main (int argc, char **argv)
 
 	XtAppMainLoop(app);
 	return status;
+}
+
+void catch_events(Widget shell, XtPointer client_data, XEvent *event, Boolean *ctd)
+{
+	static Window   child_window = 0;
+	static Display *child_display = NULL;
+	if (event->type == MapRequest) {
+		XMapWindow(event->xmaprequest.display, event->xmaprequest.window);
+		child_display = event->xmaprequest.display;
+		child_window = event->xmaprequest.window;
+		printf("Map Request!\n");
+	}
+
+	if (event->type == ConfigureNotify || event->type == MapRequest) {
+		if (child_window) {
+			printf("Configure Notify!\n");
+
+	                // Get container window attributes
+			XWindowAttributes attrs;
+			XGetWindowAttributes(child_display, XtWindow(shell), &attrs);
+			printf("DIM %u  x %u\n", attrs.width, attrs.height);
+			XMoveResizeWindow(child_display, child_window, 2, 2, attrs.width - 6, attrs.height - 6);
+                }
+	}
+
+	if (event->type == DestroyNotify) {
+		if (event->xdestroywindow.window == child_window) {
+			child_window = 0;
+			printf("Child Destroyed!\n");	
+		}
+	}
+
+
 }
 
 struct _playlist_loader {
@@ -210,7 +268,6 @@ void proc_exit()
 
 void play_song_external(char *filename)
 {
-    pid_t cpid, w;
     int wstatus;
 
     if (gSignalStatus!=0)
